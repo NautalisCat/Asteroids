24a42b9bc6dfc448aaa15ae123a9677b3fd412df
NautalisCat <29218985+NautalisCat@users.noreply.github.com>
2025-02-19 19:12:47 -0500

    Added ability to draw a triangle on the screen

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    x = SCREEN_WIDTH /2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
     while True:
          for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    pygame.fill(black)
-    pygame.display.flip()
+         screen.fill((0,0,0))
+         player.draw(screen)
+         pygame.display.flip()
+         dt = game_clock.tick(60) / 1000
     
 if __name__ == "__main__":
         main()
